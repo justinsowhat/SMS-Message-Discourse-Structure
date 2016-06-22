76b205feacdb2cb04bfb23a63ca8703670014543
Justin So <justinso@Justins-MacBook-Air.local>
2016-06-22 19:03:30 -0400

    updated data set with more features

--- NeuralNetworkModel/src/main/java/RecurrentModel.java
+++ NeuralNetworkModel/src/main/java/RecurrentModel.java
@@ -30,7 +30,7 @@ public class RecurrentModel {
     private static final String FILENAME = "data.txt";
 
     // RNN dimensions
-    public static final int HIDDEN_LAYER_WIDTH = 5;
+    public static final int HIDDEN_LAYER_WIDTH = 8;
     public static final int HIDDEN_LAYER_CONT = 1;
     public static final Random r = new Random(7894);
 
@@ -39,7 +39,7 @@ public class RecurrentModel {
         String delimiter = ",";
         RecordReader recordReader = new CSVRecordReader(numLinesToSkip,delimiter);
         recordReader.initialize(new FileSplit(new ClassPathResource(FILENAME).getFile()));
-        final int numInputs = 8;
+        final int numInputs = 14;
         int outputNum = 2;
         int iterations = 10000;
         long seed = 123;
@@ -47,16 +47,14 @@ public class RecurrentModel {
         int numClasses = 2;
         int batchSize = 15192;
         DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader,batchSize,labelIndex,numClasses);
-
         DataSet next = iterator.next();
         next.normalizeZeroMeanZeroUnitVariance();
         SplitTestAndTrain testAndTrain = next.splitTestAndTrain(.80);
         DataSet trainingData = testAndTrain.getTrain();
 
-        // some common parameters
         NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
         builder.iterations(iterations);
-        builder.learningRate(0.01);
+        builder.learningRate(0.1);
         builder.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT);
         builder.seed(seed);
         builder.biasInit(0);
@@ -66,32 +64,24 @@ public class RecurrentModel {
 
         NeuralNetConfiguration.ListBuilder listBuilder = builder.list();
 
-        // first difference, for rnns we need to use GravesLSTM.Builder
         for (int i = 0; i < HIDDEN_LAYER_CONT; i++) {
             GravesLSTM.Builder hiddenLayerBuilder = new GravesLSTM.Builder();
             hiddenLayerBuilder.nIn(i == 0 ? numInputs : HIDDEN_LAYER_WIDTH);
             hiddenLayerBuilder.nOut(HIDDEN_LAYER_WIDTH);
-            // adopted activation function from GravesLSTMCharModellingExample
-            // seems to work well with RNNs
             hiddenLayerBuilder.activation("tanh");
             listBuilder.layer(i, hiddenLayerBuilder.build());
         }
 
-        // we need to use RnnOutputLayer for our RNN
         RnnOutputLayer.Builder outputLayerBuilder = new RnnOutputLayer.Builder(LossFunction.MCXENT);
-        // softmax normalizes the output neurons, the sum of all outputs is 1
-        // this is required for our sampleFromDistribution-function
         outputLayerBuilder.activation("softmax");
         outputLayerBuilder.nIn(HIDDEN_LAYER_WIDTH);
         outputLayerBuilder.nOut(outputNum);
         listBuilder.layer(HIDDEN_LAYER_CONT, outputLayerBuilder.build());
 
-        // finish builder
         listBuilder.pretrain(false);
         listBuilder.backprop(true);
         listBuilder.build();
 
-        // create network
         MultiLayerConfiguration conf = listBuilder.build();
         MultiLayerNetwork net = new MultiLayerNetwork(conf);
         net.init();
@@ -99,20 +89,13 @@ public class RecurrentModel {
 
         DataSet test = testAndTrain.getTest();
 
-        // some epochs
         for (int epoch = 0; epoch < 100; epoch++) {
 
             System.out.println("Epoch " + epoch);
 
-            // train the data
             net.fit(trainingData);
-
-            // clear current stance from the last example
             net.rnnClearPreviousState();
 
-            // run one step -> IMPORTANT: rnnTimeStep() must be called, not
-            // output()
-            // the output shows what the net thinks what should come next
             INDArray output = net.rnnTimeStep(test.getFeatureMatrix());
             Evaluation eval = new Evaluation(2);
             eval.eval(test.getLabels(), output);
